bf918edc56dbc4bad95e83e0a90e74192ee16f14
Perviz Veliyev <veliyevperviz290@gmail.com>
2023-10-20 10:41:19 +0400

    Add project files.

---
+++ Controllers/VillaAPIController.cs
@@ -0,0 +1,28 @@
+﻿using MagicVilla_VillaAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController: ControllerBase
+    {
+
+        public IEnumerable<Villa>getVilllas()
+        {
+
+            return new List<Villa>
+            {
+                new Villa {Id=1,Name="Miau"},
+                new Villa {Id=2,Name="auye"}
+            };
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Model/Villa.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_VillaAPI.Model
+{
+    public class Villa
+    {
+
+
+        public int Id { get; set; }        
+        public string Name { get; set; }    
+    }
+    }
+
